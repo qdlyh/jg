8681e39c1565c294cc07d77f47a781cfa6461b47
qdlyh <liang620523@163.com>
2018-07-11 18:09:24 +0800

    message

---
+++ hl/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="stylus">
+* {
+  padding: 0;
+  margin: 0;
+  list-style-type: none;
+  text-decoration: none;
+  box-sizing: border-box;
+  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
+}
+
+i {
+  font-style: normal;
+}
+
+#app {
+  margin: 0 auto;
+  // max-width: 750px;
+  letter-spacing: 1px;
+  -webkit-overflow-scrolling: touch; // 在ios上滑动不流畅样式设置处理
+  word-break: break-all; // 字母数字强制换行
+  // mescroll
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -webkit-tap-highlight-color: transparent;
+}
+</style>
---
+++ hl/src/components/home.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <div class="home">
+      <div class="top">
+        <img src="../assets/01.png" alt="">
+      </div>
+      <div class="middle">
+        <img src="../assets/1.png" alt="">
+        <img src="../assets/2.png" alt="">
+        <img src="../assets/3.png" alt="">
+        <img src="../assets/4.png" alt="">
+      </div>
+      <div class="bottom">
+        <img src="../assets/02.png" alt="">
+        <div class="btn">
+          <Button :inline="true">注册</Button>
+        </div>
+      </div>
+      <div class="login">
+        <div class="top">
+          <img src="../assets/logo.png" alt="">
+          <img src="../assets/5.png" alt="">
+          <i>注册</i>
+        </div>
+        <div class="middle">
+
+        </div>
+        <div class="bottom">
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import { Button } from 'cube-ui'
+export default {
+  components: {
+    Button
+  },
+  data() {
+    return {
+      toastTxt: 'cube toast content'
+    }
+  },
+  methods: {
+    showToast() {
+      this.$createToast({
+        txt: this.toastTxt
+      }).show()
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.home {
+  width: 100%;
+  background: #fff;
+
+  img {
+    width: 100%;
+    height: 100%;
+    vertical-align: top;
+  }
+
+  .top {
+    position: relative;
+    top: 0;
+    left: 0;
+  }
+
+  .middle {
+    width: 100%;
+  }
+
+  .bottom {
+    position: relative;
+    text-align: center;
+
+    .btn {
+      margin: auto;
+      position: absolute;
+      top: 65%;
+      left: 0;
+      bottom: 0;
+      right: 0;
+    }
+
+    .btn >>>button {
+      background: #FFD966;
+      color: #454545;
+      min-width: 120px;
+      margin: 0 auto;
+      font-size: 20px;
+    }
+  }
+}
+</style>
